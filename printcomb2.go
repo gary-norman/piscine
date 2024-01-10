ec562b04ae86f4745b87de02f8faf6df5a4d0075
Gary Norman <gary.norman.th@gmail.com>
2024-01-10 16:55:57 +0000

    corrected printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -6,15 +6,17 @@ func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '8'; j++ {
 			for k := '0'; k <= '9'; k++ {
-				for l := '1'; l <= '9'; l++ {
-					z01.PrintRune(i)
-					z01.PrintRune(j)
-					z01.PrintRune(' ')
-					z01.PrintRune(k)
-					z01.PrintRune(l)
-					if i < '9' || j < '8' || k < '9' || l < '9' {
-						z01.PrintRune(',')
+				for l := '0'; l <= '9'; l++ {
+					if i < k || i == k && j < l {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
 						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(l)
+						if i < '9' || j < '8' || k < '9' || l < '9' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
 					}
 				}
 			}
