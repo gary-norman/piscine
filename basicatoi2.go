b3f14a3a6d03edc2375f822a03ff0791a559104a
Gary Norman <gary.norman.th@gmail.com>
2024-01-12 12:39:46 +0000

    corrected basicatoi2 task

--- basicatoi2.go
+++ basicatoi2.go
@@ -2,6 +2,7 @@ package piscine
 
 func BasicAtoi2(s string) int {
 	var b int
+
 	for i := 0; i <= len(s)-1; i++ {
 		if s[i] == ' ' {
 			b = 0
@@ -12,6 +13,9 @@ func BasicAtoi2(s string) int {
 			if i <= len(s)-2 {
 				b *= 10
 			}
+		} else {
+			b = 0
+			break
 		}
 	}
 	return b
