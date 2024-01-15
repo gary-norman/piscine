048a9d1e530782a648c937b0cf8f4bf184739932
Gary Norman <gary.norman.th@gmail.com>
2024-01-15 19:18:38 +0000

    corrected iterativefactorial task

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,7 +1,6 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	b := 0
 	if nb == 21 {
 		return 0
 	}
@@ -9,7 +8,7 @@ func IterativeFactorial(nb int) int {
 		return 1
 	}
 	for i := nb - 1; i > 0; i-- {
-		b = nb * i
+		nb *= i
 	}
-	return b
+	return nb
 }
