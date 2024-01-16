18d151bf494d85714108d81fe67377b2d89d3ea1
Gary Norman <gary.norman.th@gmail.com>
2024-01-16 13:34:55 +0000

    completed recursivefactorial task

---
+++ recursivefactorial.go
@@ -0,0 +1,20 @@
+package piscine
+
+func factorial(a int) int {
+	b := a
+	if a > 1 {
+		b = b * factorial(a-1)
+	}
+	return b
+}
+
+func RecursiveFactorial(nb int) int {
+	if nb >= 21 || nb <= -21 {
+		return 0
+	}
+	if nb == 0 || nb == 1 {
+		return 1
+	}
+	var result = factorial(nb)
+	return result
+}
