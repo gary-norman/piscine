47c7a3db5e6b9444f6d3e783f0a7fd24e2249cab
Gary Norman <gary.norman.th@gmail.com>
2024-01-18 20:19:52 +0000

    corrected isprintable task

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(s string) bool {
 	var result bool
 	for i := 0; i < len(s); i++ {
-		if s[i] == '\\' && s[i-1] != '\\' {
+		if !(s[i] >= 31 && s[i] <= 127) {
 			result = false
 			return result
 		} else {
