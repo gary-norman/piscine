9bf808dea219e673164275017c293142e68fc1a5
Gary Norman <gary.norman.th@gmail.com>
2024-01-18 21:43:02 +0000

    corrected capitalize task

--- capitalize.go
+++ capitalize.go
@@ -1,7 +1,21 @@
 package piscine
 
+func lower(s string) string {
+	runes := []rune(s)
+	var result string
+	for i := 0; i < len(s); i++ {
+		if s[i] >= 65 && s[i] <= 90 {
+			runes[i] = rune(s[i] + 32)
+			result += string(runes[i])
+		} else {
+			result += string(runes[i])
+		}
+	}
+	return result
+}
+
 func Capitalize(s string) string {
-	s = ToLower(s)
+	s = lower(s)
 	runes := []rune(s)
 	newWord := false
 	var result string
