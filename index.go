29eb5583841b9ad9b9a8f505aa53d5912b48b2f9
Gary Norman <gary.norman.th@gmail.com>
2024-01-18 19:34:48 +0000

    corrected index task

--- index.go
+++ index.go
@@ -1,18 +1,30 @@
 package piscine
 
+// func Index(s string, toFind string) int {
+// 	result := -1
+// 	contains := 0
+// 	for index, char := range s {
+// 		for i := 0; i < len(toFind); i++ {
+// 			if rune(toFind[i]) == char {
+// 				result = index
+// 				contains++
+
+// 			}
+// 		}
+// 	}
+// 	return result
+// }
+
 func Index(s string, toFind string) int {
-	var result int
-	for index, char := range s {
-		for i := 0; i < len(toFind); i++ {
-			if rune(toFind[i]) == char {
-				if i > 0 {
-					result = index - i
-				} else {
-					result = index
-					return result
-				}
-			}
+	// if toFind is an empty string
+	if toFind == "" || s == "" {
+		return 0
+	}
+
+	for i := 0; i <= len(s)-len(toFind); i++ {
+		if s[i:i+len(toFind)] == toFind { // slices the string to check for toFind inside it
+			return i
 		}
 	}
-	return result
+	return -1
 }
