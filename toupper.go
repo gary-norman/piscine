577c071f747af71652cd0458f0dbbc84b1b6187b
Gary Norman <gary.norman.th@gmail.com>
2024-01-18 20:35:20 +0000

    completed toupper task

---
+++ toupper.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ToUpper(s string) string {
+	var runes = []rune(s)
+	var result string
+	for i := 0; i < len(s); i++ {
+		if s[i] >= 97 && s[i] <= 122 {
+			runes[i] = rune(s[i] - 32)
+			result += string(runes[i])
+		} else {
+			result += string(runes[i])
+		}
+	}
+	return result
+}
