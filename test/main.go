8044d4c2659c71d22607d233f861c154b4430949
Gary Norman <gary.norman.th@gmail.com>
2024-01-18 13:53:30 +0000

    completed appendrange task

--- test/main.go
+++ test/main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.Index("Hello!", "l"))
-	fmt.Println(piscine.Index("Salut!", "alu"))
-	fmt.Println(piscine.Index("Ola!", "hOl"))
+	fmt.Println(piscine.AppendRange(5, 10))
+	fmt.Println(piscine.AppendRange(10, 5))
 }
