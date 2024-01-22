4363abd6112a4bb7d57dbc0d12fb339a1a9f6ea2
Clement Denis <dev@01-edu.org>
2024-01-22 17:58:05 +0000

    Update 'capitalize.go'

--- capitalize.go
+++ capitalize.go
@@ -17,18 +17,21 @@ func lower(s string) string {
 func Capitalize(s string) string {
 	s = lower(s)
 	runes := []rune(s)
-	newWord := false
+	newWord := true
 	var result string
 	for i := 0; i < len(s); i++ {
-		if (i == 0 || newWord) {
-			if (runes[i] >= 97 && runes[i] <= 122) {
-              runes[i] = rune(s[i] - 32)
-              result += string(runes[i])
-              newWord = false
-              } else {
-                  result += string(runes[i])
-              }
-		} else if !(s[i] >= 97 && s[i] <= 122 || s[i] >= 65 && s[i] <= 90 || s[i] >= 48 && s[i] <= 57) {
+		if newWord {
+			if runes[i] >= 97 && runes[i] <= 122 {
+				runes[i] = rune(s[i] - 32)
+				result += string(runes[i])
+				newWord = false
+			} else {
+				result += string(runes[i])
+			}
+			if runes[i] >= 48 && runes[i] <= 57 {
+				newWord = false
+			}
+		} else if !((s[i] >= 97 && s[i] <= 122) || (s[i] >= 65 && s[i] <= 90) || (s[i] >= 48 && s[i] <= 57)) {
 			result += string(runes[i])
 			newWord = true
 		} else {
