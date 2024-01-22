e2dd1ea3ff4e2227521e962944543881c5228d3f
Gary Norman <gary.norman.th@gmail.com>
2024-01-22 22:03:20 +0000

    corrected point task

--- point/main.go
+++ point/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"piscine"
-
 	"github.com/01-edu/z01"
 )
 
@@ -16,14 +14,42 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+	}
+	SetNbr(n)
+}
+
+func SetNbr(n int) {
+	var i int
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	i = (n % 10)
+	if n/10 != 0 {
+		SetNbr(n / 10)
+	}
+	if i < 0 {
+		i = i * -1
+	}
+	z01.PrintRune(rune(i) + 48)
+}
+
+func PrintStr(s string) {
+	for i := 0; i < len(s); i++ {
+		z01.PrintRune(rune(s[i]))
+	}
+}
+
 func main() {
 	points := &point{}
 	setPoint(points)
-	piscine.PrintStr("x = ")
-	piscine.PrintNbr(points.x)
-	piscine.PrintStr(", y = ")
-	piscine.PrintNbr(points.y)
+	PrintStr("x = ")
+	PrintNbr(points.x)
+	PrintStr(", y = ")
+	PrintNbr(points.y)
 	z01.PrintRune('\n')
-
 	// fmt.Printf("x = %d, y = %d\n", points.x, points.y)
 }
