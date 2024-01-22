5eb093ce7eaaedd6b2be423d60fcd78025d05b80
Gary Norman <gary.norman.th@gmail.com>
2024-01-22 20:28:44 +0000

    completed trimatoi task

---
+++ trimatoi.go
@@ -0,0 +1,28 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	num := 0
+	negative := false
+	counter := 0
+	multiplier := 0
+	for _, char := range s {
+		if char == 45 {
+			negative = true
+		}
+		if char > 48 && char <= 57 {
+			if counter == 0 {
+				num += (int(char - 48))
+				multiplier = 10
+			} else {
+				num *= multiplier
+				num += (int(char - 48))
+			}
+			counter++
+		}
+	}
+	if negative {
+		return num * -1
+	} else {
+		return num
+	}
+}
