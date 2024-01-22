81f447c6d590012260c12690bb7c409c277b0c59
Gary Norman <gary.norman.th@gmail.com>
2024-01-22 18:00:33 +0000

    completed capitalize task

--- capitalize.go
+++ capitalize.go
@@ -17,14 +17,21 @@ func lower(s string) string {
 func Capitalize(s string) string {
 	s = lower(s)
 	runes := []rune(s)
-	newWord := false
+	newWord := true
 	var result string
 	for i := 0; i < len(s); i++ {
-		if i == 0 || newWord {
-			runes[i] = rune(s[i] - 32)
-			result += string(runes[i])
-			newWord = false
-		} else if !(s[i] >= 33 && s[i] <= 127) {
+		if newWord {
+			if runes[i] >= 97 && runes[i] <= 122 {
+				runes[i] = rune(s[i] - 32)
+				result += string(runes[i])
+				newWord = false
+			} else {
+				result += string(runes[i])
+			}
+			if runes[i] >= 48 && runes[i] <= 57 {
+				newWord = false
+			}
+		} else if !((s[i] >= 97 && s[i] <= 122) || (s[i] >= 65 && s[i] <= 90) || (s[i] >= 48 && s[i] <= 57)) {
 			result += string(runes[i])
 			newWord = true
 		} else {
@@ -33,3 +40,19 @@ func Capitalize(s string) string {
 	}
 	return result
 }
+
+// func Capitalize(s string) string {
+// 	runes := []rune(s)
+// 	for i, chat := range runes {
+// 		if chat < 97 || chat > 122 {
+// 			for j := 0; j < 26; j++ {
+// 				if i < len(runes)-1 {
+// 					if runes[i+1] == rune(j+97) {
+// 						runes[i+1] = rune(j + 65)
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return string(runes)
+// }
