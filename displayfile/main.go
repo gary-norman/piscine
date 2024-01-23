c7eca2a07d05b9498e123d5397805b26ac76d9f1
Gary Norman <gary.norman.th@gmail.com>
2024-01-23 18:25:08 +0000

    corrected task

--- displayfile/main.go
+++ displayfile/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
-	// import file from argument
-	file, err := os.Open(os.Args[len(os.Args)-1])
-	// print error message
-	if err != nil {
-		fmt.Printf("%v", err.Error())
+	// initialise argsLen variable as number of arguments
+	argslen := len(os.Args)
+	// initialise variable as filename if 1 argument exists
+	if argslen == 1 {
+		fmt.Println("File name missing")
+	} else if argslen > 2 {
+		fmt.Println("Too many arguments")
+	} else {
+		fileName, err := ioutil.ReadFile(os.Args[1])
+		if err != nil {
+			fmt.Printf("%v", err.Error())
+		}
+		fmt.Printf("%s", fileName) // orint the contents of the file
 	}
 	// print "content variable"
-	fmt.Println(file.Stat())
 }
