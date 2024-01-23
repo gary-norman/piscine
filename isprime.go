8109269223730e77b63a07e97c9b9cd71c1fd970
Gary Norman <gary.norman.th@gmail.com>
2024-01-23 20:42:48 +0000

    corrected isprime

--- isprime.go
+++ isprime.go
@@ -1,14 +1,11 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	// return false if nb is 1
-	if nb == 1 {
+	// return false if nb is 1, 0, or negative
+	if nb < 2 {
 		return false
 	}
-	if nb < 0 {
-		return false
-	}
-	// return true if nb is prime, else false
+	// return false if nb is cleanly divisible by any number other than nb
 	for i := 2; i < nb; i++ {
 		if nb%i == 0 {
 			return false
