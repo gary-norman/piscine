9702d9c1d446b64074172e3a1f446580ccdd67eb
Gary Norman <gary.norman.th@gmail.com>
2024-01-23 14:00:01 +0000

    displayfile 1st attempt

---
+++ displayfile/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	// import file from argument
+	file, err := os.Open(os.Args[1])
+	// print error message
+	if err != nil {
+		fmt.Printf("%v", err.Error())
+	}
+	// print "content variable"
+	println(file.Stat())
+}
