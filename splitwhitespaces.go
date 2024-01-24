fea0d27f970b7de6168424289d2e3aeedf75ab24
Gary Norman <gary.norman.th@gmail.com>
2024-01-24 20:01:40 +0000

    corrected splitwhitespaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -3,13 +3,16 @@ package piscine
 func SplitWhiteSpaces(s string) []string {
 	result := make([]string, 1)
 	wordcount := 0
+	newword := false
 	for _, char := range s {
 		if string(char) != " " {
 			result[wordcount] += string(char)
+			newword = false
 		} else {
-			if string(char) == " " && string(char-1) != " " || string(char) == "	" || string(char) == "\n" {
+			if (string(char) == " " && !newword) || string(char) == "	" || string(char) == "\n" {
 				result = append(result, "")
 				wordcount++
+				newword = true
 			}
 		}
 	}
