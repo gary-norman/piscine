7a7623f513fb78988dd909b5e25c038b09fbcc5c
Gary Norman <gary.norman.th@gmail.com>
2024-01-25 17:32:04 +0000

    corrected issorted

--- test/main.go
+++ test/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	a1 := []int{0, 1, 2, 3, 4, 5}
-	a2 := []int{0, 2, 1, 3}
+	a2 := []int{-532531, -452684, -213249, -140744, -136193, 181215, 483571, 545464}
 
 	result1 := piscine.IsSorted(f, a1)
 	result2 := piscine.IsSorted(f, a2)
