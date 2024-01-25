dc627d3492ddc8dbc4180699ae976a42f6b2d74a
Gary Norman <gary.norman.th@gmail.com>
2024-01-25 18:48:51 +0000

    corrected issorted

--- issorted.go
+++ issorted.go
@@ -10,14 +10,11 @@ func IsSorted(F func(a, b int) int, slice []int) bool {
 	for i := 0; i < len(slice)-1; i++ {
 		if F(slice[i], slice[i+1]) <= 0 {
 			counterFwd++
-			// fmt.Printf("f) i[%v]:%v - \n", i, counterFwd)
 		}
 		if F(slice[i], slice[i+1]) >= 0 {
 			counterBkwd++
-			// fmt.Printf("b) i[%v]:%v - \n", i, counterBkwd)
 		}
 	}
-	// print("\nafter for loop\n")
 	slices := len(slice) - 1
 	if counterFwd == slices || counterBkwd == slices {
 		return true
