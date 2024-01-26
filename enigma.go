853c7be10595d5db67dfe8fea2c4c5ac123c9aa1
Gary Norman <gary.norman.th@gmail.com>
2024-01-26 09:44:50 +0000

    completed enigma

---
+++ enigma.go
@@ -0,0 +1,19 @@
+package piscine
+
+// This function will put :
+//     a into c
+//     c into d
+//     d into b
+//     b into a
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	w := ***a
+	x := *b
+	y := *******c
+	z := ****d
+
+	***a = x
+	*******c = w
+	****d = y
+	*b = z
+}
